b3e2a948acbd653ef4cc846954aa2a5a4a2f35d7
Braeden Drennan <aucklanduni332@gmail.com>
2023-12-14 11:53:59 +1300

    Added video capability

--- difference_detection/components/image_display.py
+++ difference_detection/components/image_display.py
@@ -13,11 +13,51 @@ class ImageDisplay(QWidget):
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.label)
 
-    def displayImage(self, image_path):
-        image = cv2.imread(image_path)
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        qimage = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-        pixmap = QPixmap.fromImage(qimage)
-        self.label.setPixmap(pixmap.scaled(500, 500, Qt.KeepAspectRatio))
+    def displayImage(self, image_path):  
+        # Load the image from the file  
+        image = cv2.imread(image_path)  
+        # Check if the image was correctly loaded  
+        if image is None:  
+            print(f"Failed to load image at {image_path}")  
+            return  
+        # Convert the image to RGB color space  
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+        # Save the image  
+        self.saveImage(rgb_image, image_path)  
+        # Display the image  
+        h, w, ch = rgb_image.shape  
+        bytes_per_line = ch * w  
+        qimage = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)  
+        pixmap = QPixmap.fromImage(qimage)  
+        self.label.setPixmap(pixmap.scaled(500, 500, Qt.KeepAspectRatio))  
+
+
+    def displayVideoSymbol(self, video_path):
+        # Load the video
+        cap = cv2.VideoCapture(video_path)
+        # Check if video opened successfully
+        if (cap.isOpened()== False): 
+            print("Error opening video file")
+            return
+
+        # Read until video is completed
+        if cap.isOpened():
+            # Read first frame
+            ret, frame = cap.read()
+            if ret == True:
+                # Save first frame as image
+                frame_path = '.testImages.firstframe.jpg'
+                cv2.imwrite(frame_path, frame)
+                # Display image
+                self.displayImage(frame_path)
+            else:
+                print("Can't read video file")
+        # Release video object
+        cap.release()
+
+            
+    def saveImage(self, image, image_path):  
+        # Create a new file path  
+        save_path = image_path.replace('.jpg', '_rgb.jpg')  
+        # Save the image  
+        cv2.imwrite(save_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))  
--- difference_detection/main_window.py
+++ difference_detection/main_window.py
@@ -1,41 +1,71 @@
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QPushButton
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt
-from .components.image_drop_box import ImageDropBox
+from .components.file_drop_box import FileDropBox
 from .components.image_display import ImageDisplay
-from .image_analysis import ImageAnalyzer
+from .processing.image_analysis import ImageAnalyzer
+from .processing.video_processor import VideoProcessor
 import cv2
-
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle("Image Difference Analyzer")
-        widget = QWidget()
-        self.layout = QVBoxLayout(widget)
-        self.dropbox1 = ImageDropBox(self)
-        self.dropbox2 = ImageDropBox(self)
-        self.layout.addWidget(self.dropbox1)
-        self.layout.addWidget(self.dropbox2)
-        self.button = QPushButton('Analyze Images', self)
-        self.button.clicked.connect(self.analyze_images)
-        self.layout.addWidget(self.button)
-        self.resultDisplay = ImageDisplay(self)  # This is only for displaying the result
-        self.layout.addWidget(self.resultDisplay)
-        self.setCentralWidget(widget)
-
-    def analyze_images(self):
-        analyzer = ImageAnalyzer(self.dropbox1.image_path, self.dropbox2.image_path)
-        analyzer.create_superpixels(n_segments=500)
-        result = analyzer.highlight_differences(threshold=30)
-        self.displayImage(result)
-
+import os
+ 
+  
+class MainWindow(QMainWindow):  
+    def __init__(self, parent=None):  
+        super().__init__(parent)  
+        self.initUI()  
+  
+    def initUI(self):  
+        self.setWindowTitle("File Difference Analyzer")  
+        widget = QWidget()  
+        self.layout = QVBoxLayout(widget)  
+        self.dropbox1 = FileDropBox(self)  
+        self.dropbox2 = FileDropBox(self)  
+        self.layout.addWidget(self.dropbox1)  
+        self.layout.addWidget(self.dropbox2)  
+        self.button = QPushButton('Analyze Files', self)  
+        self.button.clicked.connect(self.analyze_files)  
+        self.layout.addWidget(self.button)  
+        self.resultDisplay = ImageDisplay(self)  
+        self.layout.addWidget(self.resultDisplay)  
+        self.setCentralWidget(widget)  
+  
+    def analyze_files(self):    
+        file1 = self.dropbox1.file_path    
+        file2 = self.dropbox2.file_path    
+    
+        # Check file types and process accordingly  
+        if self.is_video(file1):  
+            processor1 = VideoProcessor(file1)  
+            image1 = processor1.combine_frames()  
+        else:  
+            image1 = cv2.imread(file1)  
+    
+        if self.is_video(file2):    
+            processor2 = VideoProcessor(file2)    
+            image2 = processor2.combine_frames()  
+        else:  
+            image2 = cv2.imread(file2)  
+    
+        # Ensure images are successfully loaded/created  
+        if image1 is None or image2 is None:  
+            print("Failed to get image")  
+            return  
+    
+        analyzer = ImageAnalyzer(image1, image2)  
+        analyzer.create_superpixels(n_segments=500)    
+        result = analyzer.highlight_differences(threshold=30)    
+    
+        self.displayImage(result)  
+  
     def displayImage(self, image):  
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
         h, w, ch = rgb_image.shape  
         bytes_per_line = ch * w  
         qimage = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)  
         pixmap = QPixmap.fromImage(qimage)  
         self.resultDisplay.label.setPixmap(pixmap.scaled(500, 500, Qt.KeepAspectRatio))  
+  
+    def is_video(self, file_path):  
+        video_extensions = {".mp4", ".avi", ".mov", ".flv", ".mkv"}  
+        _, extension = os.path.splitext(file_path)  
+        return extension.lower() in video_extensions  
